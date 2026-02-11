b728007dca20f208ca7bf051eed89bf102a3a660
wyatthaas <wyatthaas@ou.edu>
2026-02-10 19:05:38 -0600

    updates

--- hw_haas0025_wyatthaas/hw_haas0025_wyatthaas/Program.cs
+++ hw_haas0025_wyatthaas/hw_haas0025_wyatthaas/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
@@ -68,34 +70,66 @@ class Program
 	static void StudentGrade()
 	{
 		Console.Clear();
-		Console.WriteLine("--- Student Grade Calculator ---");
+		Console.WriteLine("--- Final Grade Calculator ---");
 
-		// ENTER YOUR CODE HERE
+		Console.WriteLine("Enter your first and last name: ");
+		string FirstLastName = Console.ReadLine();
 
-		// final grade calculator
+		Console.WriteLine("Enter your student ID number: ");
+		string StudentID = Console.ReadLine();
 
-// enter first name, last name (string)
-// enter student ID (int)
 
-// welcome, 'xxxxxx xxxxxx'
+		Console.WriteLine($"Welcome, {FirstLastName}");
+		Console.WriteLine("I will need some information to calculate your final grade.");
 
-// enter your quizzes grade qgrade (double)
-// enter participation pgrade (double)
-// enter homework hgrade (double)
-// enter midterm mgrade (double)
-// enter final fgrade (double)
+		Console.WriteLine("Enter your quizzes grade: ");
+		double qgrade = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter your participation grade: ");
+		double pgrade = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter your homework grade: ");
+		double hgrade = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter your midterm grade: ");
+		double mgrade = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter your final exam grade: ");
+		double fgrade = Convert.ToDouble(Console.ReadLine());
 
-// enter your quizzes q% (double)
-// enter participation p% (double)
-// enter homework h% (double)
-// enter midterm m% (double)
-// enter final f% (double)
+		Console.WriteLine("Enter your quizzes %: ");
+		double qp = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter your participation %: ");
+		double pp = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter your homework %: ");
+		double hp = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter your midterm %: ");
+		double mp = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter your final exam %: ");
+		double fp = Convert.ToDouble(Console.ReadLine());
 
-// calculate final grade
-// qgrade*q% + pgrade*p% + hgrade*h% + mgrade*m% + fgrade*f% = finalgrade
-// finalgrade= 'xxx'%;
+		string finalgrade = Convert.ToString(qgrade * qp + pgrade * pp + hgrade * hp + mgrade * mp + fgrade * fp);
 
-// output finalgrade 'xxx'%;
+		Console.WriteLine($"Your final grade is {finalgrade}%.");
+
+		// enter first name, last name (string)
+		// enter student ID (int)
+
+		// welcome, 'xxxxxx xxxxxx'
+
+		// enter your quizzes grade qgrade (double)
+		// enter participation pgrade (double)
+		// enter homework hgrade (double)
+		// enter midterm mgrade (double)
+		// enter final fgrade (double)
+
+		// enter your quizzes q% (double)
+		// enter participation p% (double)
+		// enter homework h% (double)
+		// enter midterm m% (double)
+		// enter final f% (double)
+
+		// calculate final grade
+		// qgrade*q% + pgrade*p% + hgrade*h% + mgrade*m% + fgrade*f% = finalgrade
+		// finalgrade= 'xxx'%;
+
+		// output finalgrade 'xxx'%;
 
 		Console.WriteLine("\nPress any key to return to menu...");
 		Console.ReadKey();
